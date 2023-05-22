5e7550047a14452afe32732f02800aefa8ece160
Ferrorist <ferrorist@naver.com>
2023-05-22 15:16:49 +0900

    create TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:protect/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -9,81 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          title: const Text(
-            "Res-Q",
-            style: TextStyle(
-              color: Colors.green,
-            ),
-          ),
-        ),
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 30,
-          ),
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        // 경상 환자를 위한 버튼 (NLP)
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 25,
-                          ),
-                          child: Text(
-                            "증상 입력",
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 27,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        // 경상 환자를 위한 버튼 (NLP)
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 27,
-                          ),
-                          child: Text(
-                            "응급실 찾기",
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 27,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
